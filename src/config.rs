f366e3be2010c11760c6a4c9338bc4bbff3aad1e
Brixter Porras <brixterporras@gmail.com>
2023-03-13 16:45:23 +0800

    refactor: env variables parser

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,5 @@
-use serde::Serialize;
-use std::env;
-
-#[derive(Serialize)]
+#[derive(clap::Parser)]
 pub struct AppConfig {
-    pub port: u16,
-}
-
-impl AppConfig {
-    pub fn new() -> Self {
-        // for key in AppConfig::from(value) {
-        //     println!("{:?}", key);
-        // }
-        // for (key,value) in env::vars{
-        //
-        // }
-        AppConfig {
-            port: env::var("PORT")
-                .unwrap_or("8000".to_string())
-                .parse()
-                .unwrap(),
-        }
-    }
+    #[clap(long, env, default_value = "8000")]
+    pub port: u32,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use clap::Parser;
 use dotenvy::dotenv;
-use rust_rest::core::config::AppConfig;
+use rust_rest::config::AppConfig;
 use std::sync::Arc;
 
 fn main() {
     dotenv().ok();
-    let config = Arc::new(AppConfig::new());
+    let config = Arc::new(AppConfig::parse());
 
     println!("Hello, world! {:?}", &config.port);
 }
