84a039f23bffde8fdcba2393e2fb2e306b66ed48
Brixter Porras <brixterporras@gmail.com>
2023-04-01 11:15:13 +0800

    refactor: logger to daily

--- src/logger.rs
+++ src/logger.rs
@@ -17,7 +17,7 @@ pub struct Logger {}
 
 impl Logger {
     pub fn init() -> WorkerGuard {
-        let file_appender = tracing_appender::rolling::hourly("logs", "daily.log");
+        let file_appender = tracing_appender::rolling::daily("logs", "daily.log");
         let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
         tracing_subscriber::fmt()
             .with_writer(non_blocking)
